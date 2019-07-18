65ff0e62604d0362dd705fdd6ce4e07999d16c59
Ramon Vidallon <ramon.vidallon@icloud.com>
2019-07-18 16:57:52 -0700

    refined code in eventCard.js

--- client/src/components/eventcard/index.js
+++ client/src/components/eventcard/index.js
@@ -1,4 +1,4 @@
-import React from './node_modules/react';
+import React from 'react';
 import './style.css';
 
 const eventCard = (props) => {
@@ -11,41 +11,24 @@ const eventCard = (props) => {
       className="eventCard__wrap"
       onClick={clickHandler}
     >
-      <Card border="dark" style={{ width: '18rem' }}>
-    <Card.Header>Events near you</Card.Header>
-    <Card.Body>
-      <Card.Title>{props.title}</Card.Title>
-      <Card.Text>
-       {props.body}
-      </Card.Text>
-    </Card.Body>
-  </Card>
-    </div>
+        <div className="eventCard">
+          <div className="eventCard-inner">
+            <div className="eventCard-front">
+              <div className="card-header">Event you might be interested in..</div>
+              <div className="card-body text-dark">
+                <h5 className="card-title">{props.eventCard__title}</h5>
+                <img src="imgs/terchplaceholderpic.jpeg" alt="tech pic" width="auto" height="auto">
+                <p id="eventCard__text" className="card-text">{props.eventCard__content}</p>
+              </div>
+            </div>
+
+            <div className="eventCard-back">
+              <button id="attendBtn" type="button" className="btn btn-primary">Attend</button>
+            </div>
+          </div>
+        </div>
   );
 
-  // <div class="row">
-  // <div class="col-md-4">
-  //   <div class="eventCard">
-  //     <div class="eventCard-inner">
-  //       <div class="eventCard-front">
-  //         <div class="card-header">Event you might be interested in..</div>
-  //         <div class="card-body text-dark">
-  //           <h5 class="card-title">CSS meetup</h5>
-  //           <img src="imgs/terchplaceholderpic.jpeg" alt="tech pic" width="auto" height="auto">
-  //           <p id="eventCard__text" class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-  //             eiusmod tempor incididunt ut
-  //             labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-  //             ut
-  //             aliquip ex ea commodo consequat.</p>
-  //         </div>
-  //       </div>
-
-  //       <div class="eventCard-back">
-  //         <button id="attendBtn" type="button" class="btn btn-primary">Attend</button>
-  //       </div>
-  //     </div>
-  //   </div>
-  // </div>
 }
  
 export default eventCard;
