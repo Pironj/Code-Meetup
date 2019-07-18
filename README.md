ece23b82a2771a473800a1c756d364612eb4edac
EndreoT <endreotm@gmail.com>
2019-07-18 10:07:07 -0700

    update linting method

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-#Code Meetup
+# Code Meetup
+
+## Description
+
+## Motivation
+
+## Results
+
+## Future Improvements
+
+
+## Develment Information
+
+### Run development server
+```
+npm run start
+```
+
+### Seed DB
+```
+npm run seed
+```
+
+### Lint code
+Make sure to install eslint and babel-eslint globally first:
+```
+npm i -g eslint
+npm i -g babel-eslint
+```
+### Run eslint fix
+```
+npm run fix
+```
--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 // This file exports the Input, TextArea, and FormBtn components
 
@@ -20,7 +20,7 @@ export function TextArea(props) {
 
 export function FormBtn(props) {
   return (
-    <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
+    <button {...props} style={{ float: 'right', marginBottom: 10 }} className="btn btn-success">
       {props.children}
     </button>
   );
