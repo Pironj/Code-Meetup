c6d7bdc22b25dad14e88f8ff2278d1eff8b830a9
CBirkbeck <cnbirkbeck@gmail.com>
2019-07-18 19:21:12 -0700

    updated btn component, removed col, container, and row as they are already defined in grid component

---
+++ client/src/components/btn/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Btn(props) {
+    return (
+      <span className="_btn" {...props} role="button" tabIndex="0">
+      </span>
+    );
+  }
+  
+  export default Btn;
