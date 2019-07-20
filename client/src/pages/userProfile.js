af1ff6ada9df2dab407f795a24568ed496c67c19
CBirkbeck <cnbirkbeck@gmail.com>
2019-07-20 12:46:48 -0700

    set up initial state and mounting event card

--- client/src/pages/userProfile.js
+++ client/src/pages/userProfile.js
@@ -5,3 +5,23 @@ import Nav from "../components/Nav";
 import EventCard from "../components/eventcard";
 import Axios from "axios";
 import { Col, Row, Container } from "../components/Grid";
+
+class UserProfile extends React.Component{
+    state = {
+        events: [],
+    }
+
+    componentDidMount() {
+        Axios.get("/api/events")
+        .then(res =>{
+            console.log(res.data);
+            this.setState({events:res.data})
+        }).catch(err=>console.log(err))
+    }
+
+    renderEventCards = () => {
+        this.state.events.map(event => (<EventCard eventTitle={event.Title} eventContent={event.description} key={event._id}/>))
+    }
+
+    
+}
