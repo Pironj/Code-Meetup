d798f147433fd6035a8e465b0234003a4a476759
CBirkbeck <cnbirkbeck@gmail.com>
2019-07-20 12:32:26 -0700

    imported components for user profile

---
+++ client/src/pages/userProfile.js
@@ -0,0 +1,7 @@
+import React from "react";
+import API from "../utils/API";
+import Btn from "../components/btn";
+import Nav from "../components/Nav";
+import EventCard from "../components/eventcard";
+import Axios from "axios";
+import { Col, Row, Container } from "../components/Grid";
