ce89ec4fdc72e42df5a9f2756aa4e73599efdba7
Ramon Vidallon <ramon.vidallon@icloud.com>
2019-07-21 15:20:04 -0700

    styling/ component routing changes

--- client/src/components/btn/index.js
+++ client/src/components/btn/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import './style.css';
 
-export const attendBtn = (props) => {
+export const AttendBtn = (props) => {
   const clickHandler = () => {
 
   }
@@ -10,7 +10,7 @@ export const attendBtn = (props) => {
     <div className="attendBtn"
     onClick={clickHandler}
     >
-      <Button variant="dark">Primary</Button>
+      <Button variant="dark">Attend</Button>
     </div>
   )
 }
