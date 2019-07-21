b2d1a7d4a0a1a4f50f7b581a6fc61aec780dc8f8
CBirkbeck <cnbirkbeck@gmail.com>
2019-07-21 12:41:03 -0700

    adding event created component to user profile page

--- client/src/pages/userProfile.js
+++ client/src/pages/userProfile.js
@@ -6,49 +6,73 @@ import EventCard from "../components/eventcard";
 import Axios from "axios";
 import { Col, Row, Container } from "../components/Grid";
 
-class UserProfile extends React.Component{
+class UserProfile extends React.Component {
     //create state
     state = {
-        user:{},
+        user: {},
         events: [],
     };
 
     //when this component mounts it grabs the user by their user id
     componentDidMount() {
         API.findUserById(this.props.match.params.id)
-        .then(res => {
-          console.log(res.data)
-          this.setState({user: res.data})
-        }).catch(err => {
-          console.log(err)
-        });
+            .then(res => {
+                console.log(res.data)
+                this.setState({ user: res.data })
+            }).catch(err => {
+                console.log(err)
+            });
 
         //and gets all the event's in the database that user created
-       API.getAllUserEvents(this.props.match.params.id)
-        .then(res =>{
-            console.log(res.data);
-            this.setState({events:res.data})
-        }).catch(err=>console.log(err))
+        API.getAllUserEvents(this.props.match.params.id)
+            .then(res => {
+                console.log(res.data);
+                this.setState({ events: res.data })
+            }).catch(err => console.log(err))
 
         //gets all event's user has saved to attend from the db
 
         API.findEventsForUser(this.props.match.params.id)
-        .then(res => {
-            console.log(res.data);
-            this.setState({events:res.data})
-        }).catch(err=>console.log(err))
+            .then(res => {
+                console.log(res.data);
+                this.setState({ events: res.data })
+            }).catch(err => console.log(err))
     };
 
     renderEventCards = () => {
-        this.state.events.map(event => (<EventCard eventTitle={event.Title} eventContent={event.description} key={event._id}/>))
+        this.state.events.map(event => (<EventCard eventTitle={event.Title} eventContent={event.description} key={event._id} />))
     }
 
     render() {
         return (
             <Container fluid>
-                <Row>
-                    <Col size = ""></Col>
-                </Row>
+                <Nav />
+                <Container>
+                    <Row>
+                        <Col size="4">
+                            <image>User Img Here</image>
+                            <React.Fragment>
+                                <td>Name: {`${user.first_name} ` + `${user.last_name}`}</td>
+                                <td> Email: {`${user.email}`}</td>
+                            </React.Fragment>
+
+                        </Col>
+                        <Col size="8">
+                            <h3>Event's You've Created</h3>
+                            <Row>
+                                <Col>
+                                    {this.state.events.map(event => (<EventCard id={event._id} eventTitle={event.title} eventContent={event.description} key={event._id} />))}
+                                </Col>
+                                <Col>
+                                    {this.state.events.map(event => (<EventCard id={event._id} eventTitle={event.title} eventContent={event.description} key={event._id} />))}
+                                </Col>
+                                <Col>
+                                    {this.state.events.map(event => (<EventCard id={event._id} eventTitle={event.title} eventContent={event.description} key={event._id} />))}
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
             </Container>
         )
     }
