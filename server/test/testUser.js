f0f1246d815eb001709233679f24b683916117b1
EndreoT <endreotm@gmail.com>
2019-07-22 10:28:47 -0700

    finish User tests

--- server/test/testUser.js
+++ server/test/testUser.js
@@ -3,22 +3,16 @@ process.env.NODE_ENV = 'test';
 
 //Require the dev-dependencies
 const chai = require('chai');
-const chaiHttp = require('chai-http');
 const server = require('../../server');
-const should = chai.should();
-
 const request = require('supertest');
-const expect = chai.expect;
-
 const db = require('../models');
 const usersSeed = require('../scripts/usersSeed.json');
 
+const should = chai.should();
+const expect = chai.expect;
 
-// chai.use(chaiHttp);
-
-const google_id = '123456';
 const user = {
-  google_id,
+  google_id: '123456',
   first_name: 'first',
   last_name: 'last',
   picture: 'pictureUrl',
@@ -41,79 +35,80 @@ describe('User', () => {
   describe('/GET /api/users', () => {
     it('it should GET all the users', async () => {
       const res = await request(server).get('/api/users');
-      res.should.have.status(200);
+      expect(res.status).to.equal(200);
       res.body.should.be.a('array');
-      res.body.length.should.be.eql(6);
+      expect(res.body.length).to.equal(6);
     });
   });
 
+  describe('GET /api/users/:id', () => {
+    it('it should GET a User by the given id', async () => {
+      const newUser = new db.User(user);
+      const savedUser = await newUser.save();
+      const res = await request(server).get(`/api/users/${savedUser._id}`);
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.property('first_name');
+      expect(res.body).to.have.property('last_name');
+      expect(res.body).to.have.property('email');
+      expect(res.body).to.have.property('picture');
+      expect(res.body).to.have.property('_id', savedUser._id.toString());
+    });
 
+    it('it should raise a 404 error with an invalid user id', async () => {
+      const res = await request(server).get('/api/users/1');
+      expect(res.status).to.equal(404);
+    });
 
-  /*
-  * Test the /GET route
-  */
-  // describe('/GET /api/users', () => {
-  //   it('it should GET all the users', async () => {
-  //     chai.request(server)
-  //       .get('/api/users')
-  //       // .end((err, res) => {
-  //       //   res.should.have.status(200);
-  //       //   res.body.should.be.a('array');
-  //       //   res.body.length.should.be.eql(7);
-  //       //   done();
-  //       // })
-  //       .then(function (res) {
-  //         res.should.have.status(201);
-  //       })
-  //       .catch(function (err) {
-  //         throw err;
-  //       });
-  //     // .catch((error) => {
-  //     //   throw (error);
+    it('it should return null if user is not found with a valid user id', async () => {
+      const res = await request(server).get('/api/users/111111111111111111111111');
+      expect(res.status).to.equal(200);
+    });
+  });
 
-  //     // });
-  //   });
-  // });
+  describe('POST /api/users', () => {
+    it('should return user when the all request body is valid', async () => {
+      const res = await request(server)
+        .post('/api/users')
+        .send(user);
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.property('google_id', user.google_id);
+      expect(res.body).to.have.property('first_name', 'first');
+    });
 
-  // describe('/POST /api/users', () => {
-  //   it('it should create a user', (done) => {
+    // add more tests to validate request body accordingly eg, make sure name is more than 3 characters etc
+  });
 
-  //     chai.request(server)
-  //       .post('/api/users')
-  //       .send(user)
-  //       .end((err, res) => {
-  //         res.should.have.status(200);
-  //         res.body.should.be.a('object');
-  //         res.body.should.have.property('google_id').eql(google_id);
-  //         done();
-  //       });
-  //   });
+  describe('PUT /:id', () => {
+    it('should update the existing order and return 200', async () => {
+      const newUser = new db.User(user);
+      await newUser.save();
 
-  //   describe('GET /api/users/:id', () => {
-  //     it('it should GET a User by the given id', (done) => {
-  //       const newUser = new db.User(user);
-  //       newUser.save((err, savedUser) => {
-  //         chai.request(server)
-  //           .get(`/api/users/${savedUser._id}`)
-  //           .send(savedUser)
-  //           .end((err, res) => {
-  //             res.should.have.status(200);
-  //             res.body.should.be.a('object');
-  //             // res.body.should.have.property('google_id');
-  //             res.body.should.have.property('first_name');
-  //             res.body.should.have.property('last_name');
-  //             res.body.should.have.property('email');
-  //             res.body.should.have.property('picture');
-  //             res.body.should.have.property('_id').eql(savedUser._id.toString());
-  //             done();
-  //           });
-  //       });
-  //     });
-  //   });
+      const res = await request(server)
+        .put('/api/users/' + newUser._id)
+        .send({
+          first_name: 'newTest',
+          email: 'newemail@gmail.com',
+        });
 
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.property('first_name', 'newTest');
+    });
+  });
 
+  describe('DELETE /:id', () => {
+    it('should delete requested id and return response 200', async () => {
+      const newUser = new db.User(user);
+      await newUser.save();
 
-  // });
+      const res = await request(server).delete('/api/users/' + newUser._id);
+      expect(res.status).to.be.equal(200);
+    });
+
+    it('should return null when deleted user does not exist', async () => {
+
+      res = await request(server).get('/api/users/111111111111111111111111');
+      expect(res.status).to.be.equal(200);
+      expect(res.body).to.be.null;
+    });
+  });
 });
-
-
