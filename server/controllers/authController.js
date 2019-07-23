dd4b6f2e45e6c7d314ae723f07428fa4ca5ba900
Julian Piron <pironj27@gmail.com>
2019-07-23 14:49:23 -0700

    edits to auth controller - still working on it

---
+++ server/controllers/authController.js
@@ -0,0 +1,92 @@
+const db = require('../models');
+const axios = require('axios');
+
+module.exports = {
+
+
+  // AUTH ROUTE
+  // Route to verify and create a user to our DB if not already present
+  verifyLogin: function (req, res, checkToken) {
+    const jwt = req.headers.authorization.slice(7);
+    // console.log(req.headers.authorization.slice(7));
+    axios.get('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + jwt)
+      .then(res => {    
+        const responseUrl = res.request.res.responseUrl.split('=');
+        const tokenRes = responseUrl[1]
+        // console.log(tokenRes);
+        checkToken(tokenRes);
+        // console.log(res.data);
+        const userInfo = res.data;
+        const checkEmail = res.data.email;
+        console.log('THIS IS YOUR EMAIL: ', checkEmail);
+        // CHECK If THE USER IN DB
+        // passing in our checkEmail varibale from our token data response and checks is User that logged in email is equal to a user in DB
+        db.User
+          .findOne({'email': checkEmail})
+          .then((currentUser) => {
+            if(currentUser) {
+              console.log(
+                '<=============== FOUND CURRENT USER IN DB ==============>\nCurrent user is:\n\n',
+                currentUser,
+                '\n========================================================\n\n'
+              );
+              
+              // TODO re-route user to homepage to avoide 404 error
+
+
+            } else {
+              // creating a new user Object from token response that matches User Schema
+              const newUser = {
+                google_id: userInfo.sub,
+                first_name: userInfo.given_name,
+                last_name: userInfo.family_name,
+                picture: userInfo.picture,
+                email: checkEmail
+              };
+
+              // Create method to save the newUser object to our DB
+              db.User
+                .create(newUser)
+                .then(mongoResponse => {
+                  console.log('created new user: ', mongoResponse);
+                  return newUser;
+                })
+                .catch(err => {
+                  console.log('\nFAILED TO CREATE NEW USER: ', err, '\n\n');
+                });
+  
+            }
+          })
+          .catch(err => {
+            console.log('Did not find user in DB error: ', err);
+          });
+          // console.log()
+      })
+      .catch(err => res.status(422).json(err));
+  
+  },
+  
+  // Check to make sure header is not undefined, if so, return Forbidden (403)
+  checkToken: (req, res, jwt, next) => {
+    console.log("JWT HEADER TOKEN\n", jwt.req.headers.authorization);
+    console.log("\n======= HEADERS =======\n\n", req);
+    const header = jwt.req.headers.authorization;
+    
+    if(typeof header !== 'undefined') {
+      // const bearer = header.split(' ');
+      // const token = bearer[1];
+      const token = jwt.req.headers.authorization.slice(7);
+      
+      
+      if (token === req) {
+        console.log("<============= TOKEN VERIFIED =============>")
+      }
+      next();
+    } else {
+        //If header is undefined return Forbidden (403)
+        res.sendStatus(403)
+    }
+  },
+
+};
+
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -19,58 +19,6 @@ module.exports = {
       .catch(err => res.status(404).json(err));
   },
 
-  // AUTH ROUTE
-  // Route to verify and create a user to our DB if not already present
-  verifyLogin: function (req, res) {
-    const jwt = req.headers.authorization.slice(7);
-    // console.log(req.headers.authorization.slice(7));
-    axios.get('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + jwt)
-      .then(res => {
-        console.log(res.data);
-        const userInfo = res.data;
-        const checkEmail = res.data.email;
-        console.log('THIS IS YOUR EMAIL: ', checkEmail);
-        // CHECK If THE USER IN DB
-        // passing in our checkEmail varibale from our token data response and checks is User that logged in email is equal to a user in DB
-        db.User
-          .findOne({'email': checkEmail})
-          .then((currentUser) => {
-            if(currentUser) {
-              console.log(
-                '<=============== FOUND CURRENT USER IN DB ==============>\nCurrent user is:\n\n',
-                currentUser,
-                '\n========================================================\n\n'
-              );
-            } else {
-              // creating a new user Object from token response that matches User Schema
-              const newUser = {
-                google_id: userInfo.sub,
-                first_name: userInfo.given_name,
-                last_name: userInfo.family_name,
-                picture: userInfo.picture,
-                email: checkEmail
-              };
-              // Create method to save the newUser object to our DB
-              db.User
-                .create(newUser)
-                .then(mongoResponse => {
-                  console.log('created new user: ', mongoResponse);
-                  return newUser;
-                })
-                .catch(err => {
-                  console.log('\nFAILED TO CREATE NEW USER: ', err, '\n\n');
-                });
-
-            }
-          })
-          .catch(err => {
-            console.log('Did not find user in DB error: ', err);
-          });
-      })
-      .catch(err => res.status(422).json(err));
-
-  },
-
   create: function (req, res) {
     db.User
       .create(req.body)
