db20c84c618ec2e6a5c4735b1bb8544c6c2bba6c
EndreoT <endreotm@gmail.com>
2019-07-25 11:01:34 -0700

    Show event creator first name in event detail view

--- client/src/pages/eventDetailPage.js
+++ client/src/pages/eventDetailPage.js
@@ -1,94 +1,78 @@
 import React from "react";
 import API from "../utils/API";
-import {AttendBtn} from "../components/btn";
+import { AttendBtn } from "../components/btn";
 import CommentBox from "../components/commentbox";
 import FullEvent from "../components/fullEvent"
 import FooterComponent from "../components/footer";
 import Axios from "axios";
-import {Container, Row, Col, Button} from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 
 
 class EventDetailsPage extends React.Component {
   state = {
     event: {},
-    id: ''
+    eventId: ''
   }
 
-    onDelete = (response) => {
-    API.deleteEvent(this.state.id)
-    .then(response => {
-      this.props.history.push('/')
-    }).catch(err => console.log(err));
-   }
+  onDelete = (response) => {
+    API.deleteEvent(this.state.eventId)
+      .then(response => {
+        this.props.history.push('/')
+      }).catch(err => console.log(err));
+  }
 
-   componentWillMount() {
-     this.setState({
-       id: this.props.match.params.id
-      });
-   }
+  componentWillMount() {
+    this.setState({
+      eventId: this.props.match.params.id
+    });
+  }
 
   componentDidMount() {
-    API.findEventById(this.state.id)
-      .then(data => {console.log(data.data
-        )
-          this.setState({
-            event: data.data
-          })
+    API.findEventById(this.state.eventId)
+      .then(data => {
+        this.setState({
+          event: data.data
+        })
       })
-      .catch (err => console.log(err))
-
+      .catch(err => console.log(err))
   }
 
   renderFullEvent = () => {
-    return(<FullEvent 
-      title={this.state.event.title} 
-      description={this.state.event.description} 
+    return (<FullEvent
+      title={this.state.event.title}
+      description={this.state.event.description}
       key={this.state.event._id}
-      creator={this.state.event.creator.first_name} 
-        
-      />
+      creator={this.state.event.creator.first_name}
+    />
     )
   }
-
-
-
 
   render() {
 
     return (
       <div>
-      <Container>
-      <Row style={{marginTop: '2rem'}}>
-    <Col>
-      {this.state.event ? this.renderFullEvent(): <p>This event does not exist</p>}
-    </Col>
-    <Col>
+        <Container>
+          <Row style={{ marginTop: '2rem' }}>
+            <Col>
+              {this.state.event._id ? this.renderFullEvent() : <p>This event does not exist</p>}
+            </Col>
+            <Col>
 
-    </Col>
-  </Row>
-      <Row > 
-      </Row>
+            </Col>
+          </Row>
+          <Row >
+          </Row>
 
-      <Row style={{marginTop: '1rem', marginLeft: '.5rem'}}> 
-      {/* <CommentBox /> */}
-      <Button onClick={this.onDelete} variant="dark">Delete</Button>
+          <Row style={{ marginTop: '1rem', marginLeft: '.5rem' }}>
+            {/* <CommentBox /> */}
+            <Button onClick={this.onDelete} variant="dark">Delete</Button>
+          </Row>
+        </Container>
 
-      </Row>
-      </Container>
-      
       </div>
-
-
-
     )
-
-
-
   }
-
-
-
 }
 
 export default EventDetailsPage;
