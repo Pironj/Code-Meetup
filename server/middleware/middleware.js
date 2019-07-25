1d3a2bee486cbf35ce530813b829d27f4dff13ba
EndreoT <endreotm@gmail.com>
2019-07-25 10:42:34 -0700

    run eslint on files

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -8,11 +8,11 @@ module.exports = {
     if (userToken) {
       next();
     } else {
-      res.redirect("/");
+      res.redirect('/');
     }
   }
 
-}
+};
 
 
 
@@ -25,13 +25,13 @@ module.exports = {
 //   console.log("JWT HEADER TOKEN\n", jwt.req.headers.authorization);
 //   console.log("\n======= HEADERS =======\n\n", req);
 //   const header = jwt.req.headers.authorization;
-  
+
 //   if(typeof header !== 'undefined') {
 //     // const bearer = header.split(' ');
 //     // const token = bearer[1];
 //     const token = jwt.req.headers.authorization.slice(7);
-    
-    
+
+
 //     if (token === req) {
 //       console.log("<============= TOKEN VERIFIED =============>")
 //       return res.sendStatus(200)
@@ -48,7 +48,7 @@ module.exports = {
 
 // // middleware function for protected routes
 // let checkToken = (req, res, next) => {
-  //   console.log('\n================ MIDDLEWARE FUNCTION ===============\n', req)
+//   console.log('\n================ MIDDLEWARE FUNCTION ===============\n', req)
 //   let token = req.headers.x-access-token || req.headers.authorization; // Express headers are auto converted to lowercase
 //   console.log(token);
 //   if (token.startsWith('Bearer ')) {
@@ -57,10 +57,10 @@ module.exports = {
 //   }
 
 //   if (token) {
-  //     jwt.verify(token, config.secret, (err, decoded) => {
+//     jwt.verify(token, config.secret, (err, decoded) => {
 //       console.log('========= TOKEN VERIFIED =========');
 //       if (err) {
-  //         return res.json({
+//         return res.json({
 //           success: false,
 //           message: 'Token is not valid'
 //         });
@@ -71,7 +71,7 @@ module.exports = {
 //     });
 //   } else {
 //     return res.json({
-  //       success: false,
+//       success: false,
 //       message: 'Auth token is not supplied'
 //     });
 //   }
