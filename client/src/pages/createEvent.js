1563a6c4bd0dae14bddbe94c3780a84a5fbddff8
andrwjrdn <andrewjordanreyes@gmail.com>
2019-07-24 18:00:25 -0700

    createEvent.js fixed

--- client/src/pages/createEvent.js
+++ client/src/pages/createEvent.js
@@ -6,6 +6,13 @@ import {Jumbotron, Button, Container, Row, Col} from "react-bootstrap";
 import { CreateBtn } from "../components/btn";
 
 class CreateEvent extends React.Component {
+  state = {
+    creator: '5d38f663f1fa3633a0109f70',
+    title: '',
+    description: '',
+    // date: '',
+};
+
   createEvent(newEvent) {
     console.log(newEvent)
   }
@@ -16,79 +23,104 @@ class CreateEvent extends React.Component {
      creator: this.refs.creator.value,
      title: this.refs.title.value,
      description: this.refs.description.value,
-     date: this.refs.date.value
+    //  date: this.refs.date.value
+
    }
 
-   console.log(newEvent);
-
-   e.preventDefault();
-   this.createEvent(newEvent);
-  }
-
-  
-
-  createEvent(newEvent) {
-    axios.request({
-      method:'put',
-      url: 'http://localhost:3000/utils/api/events/',
-      data: newEvent
-    })
-    .then(response => {
-      this.props.history.push('/utils/API')
-      console.log(response)
-    }).catch(err => console.log(err));
-  }
-
-  
-  render() {
-    
-    return (
+   API.createEvent(newEvent)
+   .then(response => {
    
-      <div>
-        <Container>
-        <Row style={{marginTop: '5rem'}}>
-            <Col>
-              
-            </Col>
-            <Col xs={4}>
-            <h3 style={{marginRight:'.5rem'}} >Create New Event</h3>
-        <form onSubmit={this.onSubmit.bind(this)}>
+    const eventId = response.data._id
+    this.props.history.push('/events/' + eventId)
+   })
 
-        <div className="input-field">
-            <input type="text" name="creator" ref="creator" />
-            <label style={{marginLeft: '.5rem', marginRight: '5rem'}} htmlFor="name">Creator</label>
-          </div>
-
-          <div className="input-field">
-            <input type="text" name="title" ref="title" />
-            <label style={{marginLeft: '.5rem', marginRight: '5rem'}} htmlFor="name">Title</label>
-          </div>
-
-          <div className="input-field">
-            <input type="text" name="description" ref="description" />
-            <label style={{marginLeft: '.5rem', marginRight: '5rem'}} htmlFor="name">Description</label>
-          </div>
-
-          <div className="input-field">
-            <input type="text" name="date" ref="date" />
-            <label style={{marginLeft: '.5rem', marginRight: '5rem'}} htmlFor="name">Date</label>
-          </div>
-          <Button variant="dark" type="submit" value="Save" className="btns">Save</Button>
-          {/* <input variant="dark" type="submit" value="Save" className="btn" /> */}
-        </form>
-            </Col>
-            <Col>
-
-            </Col>
-          </Row>
-        </Container>
-      </div>
-
-
-    )
-  }
+ 
   }
 
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.title && this.state.author) {
+      API.saveEvent({
+        title: this.state.title,
+        author: this.state.description,
+        creator: this.state.creator
+      })
+        .then(res => this.loadBooks())
+        .catch(err => console.log(err));
+    }
+  };
+
+  
+ render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <Jumbotron>
+              <h1>Create an Event</h1>
+            </Jumbotron>
+            <form>
+              <Input
+                value={this.state.title}
+                onChange={this.handleInputChange}
+                name="title"
+                placeholder="Title (required)"
+              />
+              <Input
+                value={this.state.description}
+                onChange={this.handleInputChange}
+                name="description"
+                placeholder=" Description (required)"
+              />
+              <TextArea
+                value={this.state.date}
+                onChange={this.handleInputChange}
+                name="date"
+                placeholder="Date"
+              />
+              <FormBtn
+                disabled={!(this.state.description  && this.state.title)}
+                onClick={this.handleFormSubmit}
+              >
+                Submit Event
+              </FormBtn>
+            </form>
+          </Col>
+          <Col size="md-6 sm-12">
+            <Jumbotron>
+              <h1>My Events</h1>
+            </Jumbotron>
+            {this.state.events.length ? (
+              <List>
+                {this.state.events.map(event => (
+                  <ListItem key={eventNames._id}>
+                    <Link to={"/events/" + event._id}>
+                      <strong>
+                        {event.title} by {event.author}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => this.deleteEvent(event._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 
 
 export default CreateEvent;
