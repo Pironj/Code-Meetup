676fe81f1b3cda17dd16622f08b08ce94f597c3d
EndreoT <endreotm@gmail.com>
2019-08-04 16:24:21 -0700

    merge conflicts

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,19 +3,19 @@ const userRoutes = require('./userRoutes');
 const commentRoutes = require('./commentRoutes');
 const eventRoutes = require('./eventRoutes');
 const userEventRoutes = require('./userEventRoutes');
-const auth = require('../../controllers/authController');
+
 
 // // User routes
-router.use('/users', userRoutes); // add auth routes auth.authorizeUser
+router.use('/users', userRoutes);
 
 // Event routes
 router.use('/events', eventRoutes);
 
 // Comment routes
-router.use('/comments', commentRoutes); // add auth routes auth.authorizeUser
+router.use('/comments', commentRoutes);
 
 // userEvent routes
-router.use('/userEvents', userEventRoutes); // add auth routes auth.authorizeUser
+router.use('/userEvents', userEventRoutes);
 
 
 module.exports = router;
