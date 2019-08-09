1f0088fdc5cbbfe4d5a9a4d40ab4e7cba5ce5390
EndreoT <endreotm@gmail.com>
2019-08-09 13:47:04 -0700

    simplify duplicate code in testUser.js

--- server/test/testUser.js
+++ server/test/testUser.js
@@ -5,7 +5,6 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const server = require('../../server');
 const request = require('supertest');
-const db = require('../models');
 const usersSeed = require('../scripts/usersSeed.json');
 const utils = require('../scripts/utils');
 const testUtils = require('./testUtils');
@@ -18,8 +17,10 @@ describe('User', () => {
   beforeEach(async () => { // Before each test we empty the database
     // Seed DB with users
     await utils.dropAllCollections();
-    // Add all users
-    await testUtils.seedUsers(usersSeed);
+
+    this.newUserRes = await request(server)
+      .post('/auth/signup')
+      .send(testUtils.user);
   });
 
   after(async () => {
@@ -28,23 +29,26 @@ describe('User', () => {
 
   describe('/GET /api/users', () => {
     it('it should GET all the users', async () => {
+      // Add all users
+      await testUtils.seedUsers(usersSeed);
+
       const res = await request(server).get('/api/users');
       expect(res.status).to.equal(200);
       expect(res.body).to.be.a('array');
-      expect(res.body.length).to.equal(6);
+      expect(res.body.length).to.equal(6 + 1);
     });
   });
 
   describe('GET /api/users/:id', () => {
     it('it should GET a User by the given id', async () => {
-      const savedUser = await db.User.create(testUtils.user);
-      const res = await request(server).get(`/api/users/${savedUser._id}`);
+
+      const res = await request(server).get(`/api/users/${this.newUserRes.body.user._id}`);
       expect(res.status).to.equal(200);
       expect(res.body).to.have.property('first_name');
       expect(res.body).to.have.property('last_name');
       expect(res.body).to.have.property('email');
       // expect(res.body).to.have.property('picture');
-      expect(res.body).to.have.property('_id', savedUser._id.toString());
+      expect(res.body).to.have.property('_id', this.newUserRes.body.user._id.toString());
     });
 
     it('it should raise a 422 error with an invalid user id', async () => {
@@ -61,26 +65,20 @@ describe('User', () => {
 
   describe('PUT /:id', () => {
     it('should update the existing user email and return 200 status only with validated credentials', async () => {
-      const newUserRes = await request(server)
-        .post('/auth/signup')
-        .send(testUtils.user);
 
       const authRes = await request(server)
-        .put('/api/users/' + newUserRes.body.user._id)
-        .set('Authorization', 'bearer ' + newUserRes.body.token)
+        .put('/api/users/' + this.newUserRes.body.user._id)
+        .set('Authorization', 'bearer ' + this.newUserRes.body.token)
         .send({
           email: 'newemail@gmail.com',
         });
 
       expect(authRes.status).to.equal(200);
       expect(authRes.body).to.have.property('email', 'newemail@gmail.com');
-      expect(authRes.body._id).to.equal(newUserRes.body.user._id);
+      expect(authRes.body._id).to.equal(this.newUserRes.body.user._id);
     });
 
     it('should NOT update the existing user email and return 422 status only with invalid credentials', async () => {
-      const newUserRes = await request(server)
-        .post('/auth/signup')
-        .send(testUtils.user);
 
       const otherUser = await request(server)
         .post('/auth/signup')
@@ -93,7 +91,7 @@ describe('User', () => {
 
       const authRes = await request(server)
         .put('/api/users/' + otherUser.body.user._id)
-        .set('Authorization', 'bearer ' + newUserRes.body.token)
+        .set('Authorization', 'bearer ' + this.newUserRes.body.token)
         .send({
           email: 'newemail@gmail.com',
         });
@@ -105,20 +103,14 @@ describe('User', () => {
 
   describe('DELETE /:id', () => {
     it('should delete requested id and return response 200 only with validated credentials', async () => {
-      const newUserRes = await request(server)
-        .post('/auth/signup')
-        .send(testUtils.user);
 
       const res = await request(server)
-        .delete('/api/users/' + newUserRes.body.user._id)
-        .set('Authorization', 'bearer ' + newUserRes.body.token);
+        .delete('/api/users/' + this.newUserRes.body.user._id)
+        .set('Authorization', 'bearer ' + this.newUserRes.body.token);
       expect(res.status).to.be.equal(200);
     });
 
     it('should NOT delete requested id and return 422 status only with invalid credentials', async () => {
-      const newUserRes = await request(server)
-        .post('/auth/signup')
-        .send(testUtils.user);
 
       const otherUserRes = await request(server)
         .post('/auth/signup')
@@ -131,20 +123,17 @@ describe('User', () => {
 
       const authRes = await request(server)
         .delete('/api/users/' + otherUserRes.body.user._id)
-        .set('Authorization', 'bearer ' + newUserRes.body.token);
+        .set('Authorization', 'bearer ' + this.newUserRes.body.token);
 
       expect(authRes.status).to.equal(422);
       expect(authRes.body).to.have.property('message', 'You are not authorized to perform this action');
     });
 
-    it('should NOT delete an invalied requested id and return 422 status only with invalid credentials', async () => {
-      const newUserRes = await request(server)
-        .post('/auth/signup')
-        .send(testUtils.user);
+    it('should NOT delete an invalid requested id and return 422 status only with invalid credentials', async () => {
 
       const deleteRes = await request(server)
         .delete('/api/users/111111111111111111111111')
-        .set('Authorization', 'bearer ' + newUserRes.body.token);
+        .set('Authorization', 'bearer ' + this.newUserRes.body.token);
 
       expect(deleteRes.status).to.be.equal(422);
       expect(deleteRes.body).to.have.property('message', 'You are not authorized to perform this action');
